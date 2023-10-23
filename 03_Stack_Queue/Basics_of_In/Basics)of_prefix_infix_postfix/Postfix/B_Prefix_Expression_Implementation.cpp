8ddbdfa736f3598104ad78713824cf4a3b3cc0d9
MOHAMMAD AHMAD <119584853+mohammadahmad12222686@users.noreply.github.com>
2023-10-23 20:51:34 +0530

    yes

---
+++ 03_Stack_Queue/Basics_of_In/Basics)of_prefix_infix_postfix/Postfix/B_Prefix_Expression_Implementation.cpp
@@ -0,0 +1,56 @@
+// Example
+
+// Consider the following infix expression:
+
+// (A + B) * C  (infinix meaning beech me)
+// This expression can be converted to postfix and prefix as follows:
+
+// Postfix: A B + C *      (prefix meaning operatror operand se pehle)
+// Prefix: * + A B C       (postfix meaning operatror operand ke baad me)
+
+#include<iostream>
+#include<stack>//for using stack stl
+#include<math.h>//to use functions like pow
+using namespace std;
+
+int Prefix(string str){
+    stack<int> s;
+//str.length() Returns the number of characters in the string, not including any null-termination.
+    for(int i=str.length()-1;i>=0;i--){
+        if(str[i]>='0' && str[i]<='9'){        //yaha par ASCII value hi compare hogi
+            s.push(str[i]-'0');                //integer store karwana hai
+        }
+        else
+        {
+            int op1=s.top();
+            s.pop();
+            int op2=s.top();
+            s.pop();
+            switch (str[i])
+            {
+                case '+':
+                s.push(op1 + op2);
+                break;
+                case '-':
+                s.push(op1 - op2);
+                break;
+                case '*':
+                s.push(op1 * op2);
+                break;
+                case '/':
+                s.push(op1 / op2);
+                break;
+                case '^':
+                s.push(pow(op1,op2));
+                break;
+
+            }
+            
+        }
+    }
+    return s.top();
+}
+int main(){
+    string st="-+7*45+20";
+    cout<<Prefix(st);
+}
--- 03_Stack_Queue/Basics_of_In/Basics)of_prefix_infix_postfix/Postfix/C_Postfix_Expression.cpp
+++ 03_Stack_Queue/Basics_of_In/Basics)of_prefix_infix_postfix/Postfix/C_Postfix_Expression.cpp
@@ -1,20 +1,30 @@
+// Example
+
+// Consider the following infix expression:
+
+// (A + B) * C  (infinix meaning beech me)
+// This expression can be converted to postfix and prefix as follows:
+
+// Postfix: A B + C *      (prefix meaning operatror operand se pehle)
+// Prefix: * + A B C       (postfix meaning operatror operand ke baad me)
+
 #include<iostream>
 #include<stack>//for using stack stl
 #include<math.h>//to use functions like pow
 using namespace std;
 
-int Prefix(string str){
+int Postfix(string str){
     stack<int> s;
 //str.length() Returns the number of characters in the string, not including any null-termination.
-    for(int i=str.length()-1;i>=0;i--){
+    for(int i=0;i<str.length();i++){
         if(str[i]>='0' && str[i]<='9'){        //yaha par ASCII value hi compare hogi
             s.push(str[i]-'0');                //integer store karwana hai
         }
         else
         {
-            int op1=s.top();
-            s.pop();
             int op2=s.top();
+            s.pop();
+            int op1=s.top();
             s.pop();
             switch (str[i])
             {
@@ -41,6 +51,6 @@ int Prefix(string str){
     return s.top();
 }
 int main(){
-    string st="-+7*45+20";
-    cout<<Prefix(st);
+    string st="46+2/5*7+";
+    cout<<Postfix(st);
 }
